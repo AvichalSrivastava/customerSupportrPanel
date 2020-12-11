d3d88ec609b99b1bffa7c0f422a58c78bae65b3a
Avichal Srivastava <avichal.srivastava@gaussnetworks.com>
2020-12-11 19:44:53 +0530

    adding login page

--- client/src/App.js
+++ client/src/App.js
@@ -4,4 +4,34 @@ import DashboardLayout from './layouts/DashboardLayout';
 import Login from './layouts/Login';
 import './vibe/scss/styles.scss';
 
+class App extends React.Component 
+{
+  renderLogin()
+  {
+    if(true)
+    {
+      return(
+        <Route component={Login} />
+      );
+    }
+    else
+    {
+      return(
+        <Route component={DashboardLayout} />
+      );
+    }
+  }
+
+  render()
+  {
+    return (
+      <div>
+        <BrowserRouter>
+          <Switch>
+            {this.renderLogin()}
+          </Switch>
+        </BrowserRouter>
+      </div>);
+  }
+}
 export default App;
